16c4dc9450c09f86c48f4a0c3f6dc3baa84768d7
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-04 13:30:22 +0600

    sum of not divisible by 13 between two x and y numbers

---
+++ repetition-problems/1032_Multiples_of_13.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    int sum = 0;
+
+    for (int i = x; i <= y; i++)
+    {
+        if (i % 13 != 0)
+        {
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+
+    return 0;
+}
